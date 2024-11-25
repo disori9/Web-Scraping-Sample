97ad92b186e7cc899ca346a642c8d1ea2f2ab942
Ori <disori777@gmail.com>
2024-11-25 12:08:24 +0800

    Implement extract value from id

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ def scrape(url):
     pg_source = response.text
     return pg_source
 
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    extracted_value = extractor.extract(source)["tours"]
+    return extracted_value
 
 if __name__ == "__main__":
     src = scrape(URL)
-    print(src)
+    value = extract(src)
+    print(value)
