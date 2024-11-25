87aa3babc227549c6f3fc4b9a4627441171bfda0
Ori <disori777@gmail.com>
2024-11-25 12:58:39 +0800

    Create message variable that has a subject and a body of information from the tour

--- send_email.py
+++ send_email.py
@@ -6,6 +6,17 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
 
+    tour_info = message.split(",")
+    band = tour_info[0]
+    place = tour_info[1]
+    date = tour_info[2]
+
+    message = f"""\
+Subject: New Upcoming Tour from: {band}
+
+Where: {place}
+When: {date}"""
+
     username = "violetori99@gmail.com"
     password = "adgm brzf nocs zykc"
 
