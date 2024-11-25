d8208d3aeff860a38e6ba06af30e2ac895fe2c13
Ori <disori777@gmail.com>
2024-11-25 12:25:59 +0800

    Implement feature that stores data when a new tour is announced

--- main.py
+++ main.py
@@ -14,7 +14,29 @@ def extract(source):
     extracted_value = extractor.extract(source)["tours"]
     return extracted_value
 
+def send_email():
+    print("Email was sent!")
+
+def store(data):
+    with open("data.txt", "r") as file:
+        extracted_data = file.readlines()
+
+    with open("data.txt", "w") as file:
+        extracted_data.append(data + "\n")
+        file.writelines(extracted_data)
+
 if __name__ == "__main__":
     src = scrape(URL)
     value = extract(src)
+
+    with open("data.txt", "r") as file:
+        tours = file.readlines()
+        tours = [tour.strip("\n") for tour in tours]
+
+    if value != "No upcoming tours":
+        if value not in tours:
+            send_email()
+            store(value)
+
+    print(tours)
     print(value)
