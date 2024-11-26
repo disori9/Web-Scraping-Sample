6a6b0f56ea99b30ed764f949e4799ec8374a8983
Ori <disori777@gmail.com>
2024-11-26 13:13:52 +0800

    Modify read_data and store_data functions to use a db instead of a txt

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import selectorlib
 import time
+import sqlite3
 from send_email import send_email
 
+connection = sqlite3.connect("data.db")
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
 def scrape(url):
@@ -16,26 +18,37 @@ def extract(source):
     extracted_value = extractor.extract(source)["tours"]
     return extracted_value
 
-def store(data):
-    with open("data.txt", "a") as file:
-        file.write(data + "\n")
+def store(extracted_data):
+    tour_info = extracted_data.split(",")
+    tour_info = [info.strip() for info in tour_info]
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO events VALUES(?,?,?)", tour_info)
+    connection.commit()
 
-def read_text(txt_file):
-    with open(txt_file, "r") as file:
-        data = file.readlines()
-        data = [info.strip("\n") for info in data]
+def read_data(extracted_data):
+    tour_info = extracted_data.split(",")
+    tour_info = [info.strip() for info in tour_info]
+    band, city, date = tour_info
+    cursor = connection.cursor()
+    # basically, this will return a list of tuple if there is already a band with the same information in the db
+    # otherwise it returns nothing
+    cursor.execute("SELECT * FROM events WHERE band=? AND city=? AND date=?", (band, city, date))
+    rows = cursor.fetchall()
+    return rows
 
-    return data
 
 if __name__ == "__main__":
      while True:
         src = scrape(URL)
         value = extract(src)
-
-        tours = read_text("data.txt")
-
+        print(value)
         if value != "No upcoming tours":
-            if value not in tours:
+            tours = read_data(value)
+            print(tours)
+            # here, we check if there is already a row in the db with the same info, if there is none (see read_data)
+            # returned, we store that info in the database and send an email, otherwise, the if condition doesn't
+            # start
+            if not tours:
                 send_email(value)
                 store(value)
         print(value)
